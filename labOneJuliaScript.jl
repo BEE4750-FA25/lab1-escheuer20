a88eaa938ac4f3dc3ab5fbfd28a7b846cc9e9b23
escheuer20 <hs787@cornell.edu>
2025-08-29 17:28:02 -0400

    Added script to avoid notebook crashing
    Completed part one (dot product) through separate script

---
+++ labOneJuliaScript.jl
@@ -0,0 +1,27 @@
+import Pkg
+Pkg.activate(".")
+Pkg.instantiate()
+#using Plots
+
+
+function dot_product(x, y)
+    if length(x) == length(y)
+        dot = 0
+        for i in 1:length(x)
+            dot += x[i]*y[i]
+        end
+        return dot
+
+    else
+        throw(DimensionMismatch("length of x not equal to length of y"))
+    end
+end
+##### Testing dot product
+#print(dot_product([1 2 3], [4 5 6]))
+
+dot_product([1 2 3], [4 5])
+
+
+##### Making a plot
+
+
